b3729754ba740dc2670367a99af8acd7634c730b
Phillip Valdez <Phillip0150@gmail.com>
2019-06-15 13:14:12 -0500

    Updated style.css, trek.js, and index.html

--- assets/javascript/trek.js
+++ assets/javascript/trek.js
@@ -45,12 +45,63 @@ function updateNYT(nytData){
     $("#travelSection").show();
 }
 
-function updateYelp(yelpData){
-    $("#eventsSection").show();
+function updateYelpFood(yelpData){
+    for(var i=0; i<yelpData.businesses.length ; i++){
+        //set rb as response.businesses
+        var rb = yelpData.businesses[i];
+        //set variables from the API to the variables
+        var name = rb.name;
+        var price = rb.price;
+        var rating = rb.rating; 
+        var yelpLink = rb.url;
+        var picture = rb.image_url;
+
+        $("#cardFoodImage"+i).attr("src", picture);
+        $("#cardFoodLink"+i).attr("href", yelpLink);
+        $("#cardFoodTitle"+i).text(name);
+        $("#foodPrice"+i).text(price);
+        $("#foodRating"+i).text(rating);
+        }
+    
     $("#foodSection").show();
-
-
 }
+
+function updateYelpEvent(yelpEvent){
+    console.log(yelpEvent);
+            //console data from yelp
+            
+            console.log(yelpEvent.events[1].name);
+            console.log(yelpEvent.events[1].description);
+            console.log(yelpEvent.events[1].is_free);
+            console.log(yelpEvent.events[1].event_site_url);
+            for(var j=0; j<yelpEvent.events.length ; j++){
+            
+            //  set rt as response.businesses
+            var rt = yelpEvent.events[j];
+            //set variables from the API to the variables
+            var nameEvent = rt.name;
+            var description = rt.description;
+            var free = rt.is_free;
+            var eventUrl = rt.event_site_url;
+            var imageEvent = rt.image_url;
+            $("#eventImage"+i).attr("src", imageEvent);
+            $("#eventURL"+i).attr("href", eventUrl);
+            $("#eventTitle"+i).text(nameEvent);
+            $("#descEvent"+i).text(description);
+            $("#freeEvent"+i).text("Is the event free? " +nameEvent);
+
+
+            console.log(nameEvent);
+            console.log(description);
+            console.log(free);
+            console.log(eventUrl);
+            }
+
+    $("#eventsSection").show();
+        }
+
+
+
 
 //When a button is clicked, we want to grab that value and use that value to search our apis
 $("button").on("click", function() {
@@ -64,8 +115,8 @@ $("button").on("click", function() {
     queryWeather = "https://api.openweathermap.org/data/2.5/weather?q="+userInput+",US&appid=e921e4ee26a025090f4ff9b62f27ad89";
     // querySky = "Whatever the url is"+userInput+"api key";
     queryNYT = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+userInput+"&fq=headline:(36 hours "+userInput+")&fq=section_name:(Travel)&api-key=vMdLSfd0YAZw8KWXtnoXqszuA5lKGB1T";
-    // queryYelp = "Whatever the url is"+userInput+"api key";
-
+    queryYelpFood= "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+userInput+"&limit=2"
+    queryYelpEvents = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/events?location="+userInput+"&limit=3";
     //next we are going to call our api's
     //using a ajax call, we call our url and methond
     //when we get a response, we update each section;
@@ -85,8 +136,19 @@ $("button").on("click", function() {
     }).then(updateNYT);
 
     $.ajax({
-        url: queryYelp,
+        url: queryYelpFood,
+        headers: {
+            'Authorization':'Bearer 2ozrOdoM-iqGVuP5uozgiBk6CunvT4pCllsN7PdRctZR63EopSt0ZruMP-E6Xiv7YOzffRRDGwVqUUwMLjVdKlYk_n49Q9d7WpshV0LSbgThn9oclFErTIuS14ECXXYx',
+           },
         method: "GET"
-    }).then(updateYelp);
+    }).then(updateYelpFood);
    
+
+    $.ajax({
+        url: queryYelpEvents,
+        headers: {
+            'Authorization':'Bearer 2ozrOdoM-iqGVuP5uozgiBk6CunvT4pCllsN7PdRctZR63EopSt0ZruMP-E6Xiv7YOzffRRDGwVqUUwMLjVdKlYk_n49Q9d7WpshV0LSbgThn9oclFErTIuS14ECXXYx',
+           },
+        method: "GET"
+    }).then(updateYelpFood);
 });
