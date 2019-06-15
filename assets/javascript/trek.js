e9d6d52e6ada36b2f2896378755d32d3660bcf3d
Phillip Valdez <Phillip0150@gmail.com>
2019-06-15 13:26:19 -0500

    Updated trek.js, index.html

--- assets/javascript/trek.js
+++ assets/javascript/trek.js
@@ -67,15 +67,12 @@ function updateYelpFood(yelpData){
 }
 
 function updateYelpEvent(yelpEvent){
+    console.log("In yelp event");
     console.log(yelpEvent);
-            //console data from yelp
-            
-            console.log(yelpEvent.events[1].name);
-            console.log(yelpEvent.events[1].description);
-            console.log(yelpEvent.events[1].is_free);
-            console.log(yelpEvent.events[1].event_site_url);
-            for(var j=0; j<yelpEvent.events.length ; j++){
-            
+
+        
+            for(var j=0; j<yelpEvent.events.length; j++){
+            console.log("in for loop");
             //  set rt as response.businesses
             var rt = yelpEvent.events[j];
             //set variables from the API to the variables
@@ -84,11 +81,11 @@ function updateYelpEvent(yelpEvent){
             var free = rt.is_free;
             var eventUrl = rt.event_site_url;
             var imageEvent = rt.image_url;
-            $("#eventImage"+i).attr("src", imageEvent);
-            $("#eventURL"+i).attr("href", eventUrl);
-            $("#eventTitle"+i).text(nameEvent);
-            $("#descEvent"+i).text(description);
-            $("#freeEvent"+i).text("Is the event free? " +nameEvent);
+            $("#eventImage"+j).attr("src", imageEvent);
+            $("#eventURL"+j).attr("href", eventUrl);
+            $("#eventTitle"+j).text(nameEvent);
+            $("#descEvent"+j).text(description);
+            $("#freeEvent"+j).text("Is the event free? " +free);
 
 
             console.log(nameEvent);
@@ -97,7 +94,8 @@ function updateYelpEvent(yelpEvent){
             console.log(eventUrl);
             }
 
-    $("#eventsSection").show();
+            $("#eventsSection").show();
+            console.log("show event section");
         }
 
 
@@ -115,7 +113,7 @@ $("button").on("click", function() {
     queryWeather = "https://api.openweathermap.org/data/2.5/weather?q="+userInput+",US&appid=e921e4ee26a025090f4ff9b62f27ad89";
     // querySky = "Whatever the url is"+userInput+"api key";
     queryNYT = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+userInput+"&fq=headline:(36 hours "+userInput+")&fq=section_name:(Travel)&api-key=vMdLSfd0YAZw8KWXtnoXqszuA5lKGB1T";
-    queryYelpFood= "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+userInput+"&limit=2"
+    queryYelpFood= "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location="+userInput+"&limit=2";
     queryYelpEvents = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/events?location="+userInput+"&limit=3";
     //next we are going to call our api's
     //using a ajax call, we call our url and methond
@@ -150,5 +148,5 @@ $("button").on("click", function() {
             'Authorization':'Bearer 2ozrOdoM-iqGVuP5uozgiBk6CunvT4pCllsN7PdRctZR63EopSt0ZruMP-E6Xiv7YOzffRRDGwVqUUwMLjVdKlYk_n49Q9d7WpshV0LSbgThn9oclFErTIuS14ECXXYx',
            },
         method: "GET"
-    }).then(updateYelpFood);
+    }).then(updateYelpEvent);
 });
