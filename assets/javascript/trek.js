f6644e961f21c0f08dc7c8b8d0a62e4ce5e86459
Phillip Valdez <Phillip0150@gmail.com>
2019-06-14 20:05:35 -0500

    Updated style.css, trek.js, index.html
    Added weather api and new york times api. Working

--- assets/javascript/trek.js
+++ assets/javascript/trek.js
@@ -8,7 +8,7 @@ var queryWeather;
 
 //first hiding divs
 //when we click on the submit button and get a result, we will show divs
-$("#eventSection").hide();
+$("#eventsSection").hide();
 $("#foodSection").hide();
 $("#travelSection").hide();
 $("#weatherForecast").hide();
@@ -16,7 +16,13 @@ $("#weatherForecast").hide();
 //a function to update weather
 //if we get a response from the data base, we call this function
 function updateWeather(weatherData){
-
+    var weatherTemp = ((weatherData.main.temp-273.15)* 1.80 + 32);
+    var weatherSky = weatherData.weather[0].main;
+    var weatherIcon = weatherData.weather[0].icon;
+    console.log(weatherSky);
+    $("#weatherHeader").text("Weather Forecast for " +userInput);
+    $("#tempText").text(Math.round(weatherTemp)+ " ºF ");
+    $("#tempText").append("<img src=https://openweathermap.org/img/w/"+weatherIcon+".png>");
     $("#weatherForecast").show();
 }
 
@@ -29,17 +35,18 @@ function updateSky(skyData){
 //a function to update NYT
 //if we get a response from the data base, we call this function
 function updateNYT(nytData){
-    for(var i =0; i<3; i++){
-    var article = nytData.response.docs;
-    
-    console.log(nytData.response.docs[i].web_url);
+    for(var i =0; i<4; i++){
+        var article = nytData.response.docs;
+        $("#travelURL"+i).attr("href", nytData.response.docs[i].web_url);
+        $("#headTravel"+i).text(nytData.response.docs[i].headline.main);
+
     }
 
     $("#travelSection").show();
 }
 
 function updateYelp(yelpData){
-    $("#eventSection").show();
+    $("#eventsSection").show();
     $("#foodSection").show();
 
 
@@ -54,10 +61,10 @@ $("button").on("click", function() {
     console.log(userInput);
 
     //next we want to set a query's
-    queryWeather = "Whatever the url is"+userInput+"api key";
-    querySky = "Whatever the url is"+userInput+"api key";
+    queryWeather = "https://api.openweathermap.org/data/2.5/weather?q="+userInput+",US&appid=e921e4ee26a025090f4ff9b62f27ad89";
+    // querySky = "Whatever the url is"+userInput+"api key";
     queryNYT = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+userInput+"&fq=headline:(36 hours "+userInput+")&fq=section_name:(Travel)&api-key=vMdLSfd0YAZw8KWXtnoXqszuA5lKGB1T";
-    queryYelp = "Whatever the url is"+userInput+"api key";
+    // queryYelp = "Whatever the url is"+userInput+"api key";
 
     //next we are going to call our api's
     //using a ajax call, we call our url and methond
